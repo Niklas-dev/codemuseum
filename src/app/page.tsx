69b585961482e6aaf9abbea9946c37bc3143066a
Niklas Ladehoff <business-mail-niklas@web.de>
2023-10-22 02:37:55 +0200

    fix null possible case

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,12 @@ export default async function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Link href={"/sign-in"}>Sign-In</Link>
       <pre>user: {session?.user?.name}</pre>
-      <Image src={session?.user?.image} width={100} height={100} alt="avatar" />
+      <Image
+        src={session?.user?.image!}
+        width={100}
+        height={100}
+        alt="avatar"
+      />
     </main>
   );
 }
