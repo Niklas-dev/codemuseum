24033767d4a755f66063800e64650361e90f9d89
Niklas Ladehoff <business-mail-niklas@web.de>
2023-10-23 14:40:35 +0200

    protect api route

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,9 +1,26 @@
 import { db } from "@/db";
 import { users } from "../../../db/schema";
 import { NextResponse } from "next/server";
-export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
+import { getToken } from "next-auth/jwt";
+import { IncomingMessage } from "http";
+import { NextApiRequest, NextApiResponse } from "next";
+export async function GET(req: NextApiRequest, res: NextApiResponse) {
+  const { searchParams } = new URL(req.url as string);
 
   const dbUsers = await db.select().from(users);
-  return NextResponse.json({ test: dbUsers[0] }, { status: 200 });
+
+  const token = await getToken({ req });
+  if (token) {
+    const { searchParams } = new URL(req.url as string);
+    console.log(searchParams.get("test"));
+    const dbUsers = await db.select().from(users);
+
+    return NextResponse.json({ test: dbUsers[0] }, { status: 200 });
+  } else {
+    // Not Signed in
+    return NextResponse.json(
+      { Error: "You are not signed in." },
+      { status: 401 }
+    );
+  }
 }
