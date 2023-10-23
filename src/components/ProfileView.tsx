1300af7d7c3a36ea5e9cc2c56e28f43ba8ce36ac
Niklas Ladehoff <business-mail-niklas@web.de>
2023-10-23 23:54:49 +0200

    add followers to profile page

--- src/components/ProfileView.tsx
+++ src/components/ProfileView.tsx
@@ -5,21 +5,42 @@ export default async function ProfileView() {
   return (
     <div className="px-20 py-8 w-full h-full flex flex-col z-20">
       <div className="flex flex-row gap-8">
-        <div className="bg-gray-900 w-full h-[35rem] rounded-2xl">
-          <div className="h-52 w-full bg-gray-800 rounded-t-2xl relative">
-            <Image
-              className="absolute h-32 w-32 rounded-full bg-gray-600  -bottom-14 left-8"
-              src={session!.user!.image!}
-              width={100}
-              height={100}
-              alt="avatar"
-            />
+        <div className="bg-[#161616] w-full h-[35rem] rounded-2xl flex flex-col">
+          <div className="h-52 w-full bg-[#252525] rounded-t-2xl relative">
+            <div className="absolute -bottom-14 left-8">
+              <Image
+                className=" h-32 w-32 rounded-full bg-gray-600  "
+                src={session!.user!.image!}
+                width={100}
+                height={100}
+                alt="avatar"
+              />
+            </div>
           </div>
-          <div className="pt-16 px-10">
-            <h1 className="text-2xl">{session?.user?.name}</h1>
+          <div className="pt-16 px-10 flex flex-col items-start justify-between flex-grow pb-8">
+            <div className="flex flex-col items-start  ">
+              <h1 className="text-2xl font-medium">{session?.user?.email}</h1>
+              <p className="text-lg text-gray-200 mt-4">
+                This is a bio about my profile
+              </p>
+              <p className="text-lg text-gray-300">🇩🇪 Germany, Kiel</p>
+              <div className="flex flex-row items-center gap-3 text-lg mt-3">
+                <span className="flex flex-row gap-1 text-gray-300 items-center bg-[#222222] px-3 py-2 rounded-xl">
+                  <h1 className="text-blue-400 font-bold text-xl">6032 </h1>{" "}
+                  followers
+                </span>
+                <span className="flex flex-row gap-1 text-gray-300 items-center bg-[#222222] px-3 py-2 rounded-xl">
+                  <h1 className="text-blue-400 font-bold text-xl">52 </h1>{" "}
+                  followed
+                </span>
+              </div>
+            </div>
+            <button className="flex cursor-pointer  w-fit items-center rounded-md border-2 border-black bg-violet-700 px-8 py-2 font-medium shadow-[4px_4px_0px_0px_#171717] transition-all hover:translate-x-[3px] hover:translate-y-[3px] hover:shadow-none">
+              Edit
+            </button>
           </div>
         </div>
-        <div className="w-[35rem] bg-red-300 rounded-2xl"></div>
+        <div className="w-[35rem] bg-[#161616] rounded-2xl"></div>
       </div>
     </div>
   );
