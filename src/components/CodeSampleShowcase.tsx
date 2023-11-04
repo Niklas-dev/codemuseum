89e06398db343d908fa9c983c1f97e5b95e65a27
Niklas Ladehoff <business-mail-niklas@web.de>
2023-11-04 17:17:33 +0100

    remove codeblock

--- src/components/CodeSampleShowcase.tsx
+++ src/components/CodeSampleShowcase.tsx
@@ -1,9 +1,20 @@
 "use client";
-import { CodeBlock, monokaiSublime } from "react-code-blocks";
+import hljs from "highlight.js/lib/core";
+import python from "highlight.js/lib/languages/python";
+import { useEffect, useRef } from "react";
+import "highlight.js/styles/atom-one-dark.css";
 
+hljs.registerLanguage("python", python);
 export default function CodeSampleShowcase() {
+  const codeRef = useRef<HTMLElement>(null);
+  const codeRefTwo = useRef<HTMLElement>(null);
+
+  useEffect(() => {
+    hljs.highlightBlock(codeRef.current!);
+    hljs.highlightBlock(codeRefTwo.current!);
+  }, []);
   return (
-    <div className=" h-fit  w-full flex flex-col items-center ">
+    <div className=" h-fit w-full flex flex-col items-center">
       <h1 className="text-center text-4xl font-medium whitespace-nowrap w-fit">
         Stop hiding, <br /> embrace the beauty of your code
       </h1>
@@ -14,8 +25,11 @@ export default function CodeSampleShowcase() {
       <div className="flex flex-row items-start pt-14 pb-10 gap-8">
         <div className="flex flex-col items-center">
           <h3 className="text-2xl pb-4">Code somwhere else</h3>
-          <CodeBlock
-            text={`def factorial(n):
+          <pre className="relative ">
+            <code
+              ref={codeRef}
+              className="python rounded-lg"
+            >{`def factorial(n):
         if n == 0:
             return 1
         else:
@@ -26,25 +40,21 @@ export default function CodeSampleShowcase() {
         
 num = 5
 fact = factorial(num)
-print(f"The factorial of {num} is {fact}")`}
-            language={"py"}
-            showLineNumbers={true}
-            theme={monokaiSublime}
-          />
+print(f"The factorial of {num} is {fact}")`}</code>
+          </pre>
         </div>
         <div className="flex flex-col items-center ">
           <h3 className="text-2xl pb-4">Code on CodeMuseum </h3>
-          <CodeBlock
-            text={`def factorial(n):
-        return 1 if n == 0 else n * factorial(n - 1)
-
+          <pre className="relative ">
+            <code ref={codeRefTwo} className="python rounded-lg">
+              {`def factorial(n):
+    return 1 if n == 0 else n * factorial(n - 1)
+    
 num = 5
 fact = factorial(num)
 print(f"The factorial of {num} is {fact}")`}
-            language={"py"}
-            showLineNumbers={true}
-            theme={monokaiSublime}
-          />
+            </code>
+          </pre>
         </div>
       </div>
     </div>
