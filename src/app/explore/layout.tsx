76bbe47fd5ba212d7dbd674763f9c9600ca50ef8
Niklas Ladehoff <business-mail-niklas@web.de>
2023-11-05 16:30:19 +0100

    add bottom nav bar

--- src/app/explore/layout.tsx
+++ src/app/explore/layout.tsx
@@ -1,5 +1,6 @@
 import ExploreSideBar from "@/components/ExploreSideBar";
 import type { Metadata } from "next";
+import { FaCode, FaNewspaper, FaUserFriends } from "react-icons/fa";
 
 export const metadata: Metadata = {
   title: "CodeMuseum",
@@ -14,7 +15,20 @@ export default async function ExploreLayout({
   return (
     <section className="flex flex-row justify-between relative  w-screen  h-screen overflow-x-hidden mt-20 ">
       <div className="w-[18rem] z-20 absolute lg:relative">
-        <ExploreSideBar />
+        <div className="hidden lg:flex">
+          <ExploreSideBar />
+        </div>
+        <div className="bg-[#000000] w-full h-fit py-2 bottom-0 fixed lg:hidden flex flex-row justify-around items-center text-lg">
+          <div className="flex flex-col items-center">
+            <FaCode /> Gallery
+          </div>
+          <div className="flex flex-col items-center">
+            <FaNewspaper /> Articles
+          </div>
+          <div className="flex flex-col items-center">
+            <FaUserFriends /> Community
+          </div>
+        </div>
       </div>
       <main className=" p-4 lg:p-12  h-full w-screen overflow-y-scroll z-10 flex flex-row">
         {children}
