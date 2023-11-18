787107336c7fed1c09524ced39b6dcd4c8c31326
Niklas Ladehoff <business-mail-niklas@web.de>
2023-11-18 22:28:38 +0100

    create migrations and add patch endpoint

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,9 +1,11 @@
 import { db } from "@/db";
-import { tags } from "../../../db/schema";
+import { posts, tags, updateUserSchema, users } from "../../../db/schema";
 import { NextResponse } from "next/server";
 import { JWT, getToken } from "next-auth/jwt";
-
+import { z } from "zod";
+import { eq } from "drizzle-orm";
 import { NextApiRequest, NextApiResponse } from "next";
+import { streamToJson } from "@/lib/utils";
 
 export async function GET(req: NextApiRequest, res: NextApiResponse) {
   const token = await getToken({ req });
@@ -23,9 +25,30 @@ export async function PATCH(req: NextApiRequest, res: NextApiResponse) {
   const token = await getToken({ req });
   console.log(token);
   if (token) {
-    const dbTags = await db.select().from(tags);
+    const jsonPost = await streamToJson(req.body);
 
-    return NextResponse.json(dbTags, { status: 200 });
+    try {
+      const updateUser = updateUserSchema.parse(jsonPost);
+      console.log(updateUser);
+      if (!null) {
+        return NextResponse.json(
+          { error: "Failed to create post." },
+          { status: 400 }
+        );
+      }
+
+      return NextResponse.json(null, { status: 201 });
+    } catch (err) {
+      if (err instanceof z.ZodError) {
+        return NextResponse.json(
+          err.issues.map((issue) =>
+            Object({ error: `${issue.path} ${issue.message}` })
+          ),
+          { status: 400 }
+        );
+      }
+      console.error(err);
+    }
   }
   return NextResponse.json(
     { error: "You are not signed in." },
