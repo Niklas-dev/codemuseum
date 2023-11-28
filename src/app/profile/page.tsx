1da32f4972af406d95d3b319a6128a1184cc7fa3
Niklas Ladehoff <business-mail-niklas@web.de>
2023-11-28 23:00:19 +0100

    add redirect on sign out

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,6 +1,13 @@
 import ProfileView from "@/components/ProfileView";
+import { getAuthSession } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
-export default function page() {
+export default async function page() {
+  const session = await getAuthSession();
+  if (!session?.user) {
+    return redirect("/sign-in");
+  }
+
   return (
     <div className="h-full">
       <ProfileView />
